ee6f469dabb8fa2f60be2b53626523d2ea4975a8
root <root@devopsinfra0.ra2gchi03heu5nn44a0mhrfjra.cx.internal.cloudapp.net>
2019-07-16 15:24:20 +0000

    Test for trigger

--- src/Services/Admin.java
+++ src/Services/Admin.java
@@ -3,7 +3,6 @@ package Services;
 public class Admin {
 	String username="Admin";
 	String password="password";
-	String test=null;
 	public boolean validateLogin(String userName,String password){
 		boolean valid=false;
 		if(userName.equals(username)&& this.password.equals(password)) {
